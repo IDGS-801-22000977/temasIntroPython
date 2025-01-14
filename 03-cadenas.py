038d8725b78f67011ceb072d4cf556ea1d6e955e
IDGS-801-22000977 <79349@alumnos.utleon.edu.mx>
2025-01-13 23:54:05 -0600

    Primer commit

---
+++ 03-cadenas.py
@@ -0,0 +1,16 @@
+texto1="Hola"
+texto2="Mundo"
+
+textoFiuna = texto1+ " "+texto2
+print("El saludo es: %s %s" %(texto1,texto2))
+saludoFinal2= "Saludo: {x} {y}".format(x=texto1,y=texto2)
+print(saludoFinal2)
+
+texto="desarrollo web profesional utl"
+print(texto)
+print(texto.lower)
+print(texto.upper)
+print(texto.title)
+print(texto.find("al"))
+print(texto.count("e"))
+
