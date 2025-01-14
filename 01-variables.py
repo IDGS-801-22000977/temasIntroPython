c46e027985b3f770bbf7d56a4a4a4cd6e6bf757e
IDGS-801-22000977 <79349@alumnos.utleon.edu.mx>
2025-01-13 23:51:17 -0600

    Add files via upload

---
+++ 01-variables.py
@@ -0,0 +1,13 @@
+print ("Ejemplo 1: ")
+
+num1=5
+num2=4
+res=num1+num2
+print(res)
+
+print("Dame dos numeros para sumarlos")
+print("")
+num1 = int(input("Ingresa el primer numero: "))
+num2 = int(input("Ingresa el segundo numero: "))
+suma = num1+num2
+print("La suma de {} más {} es: {}".format(num1,num2,suma))
